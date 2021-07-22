b7625f7a1b8e0d8ef0f2df1cd6b91e25223cc985
jinjupeng <im.jp@outlook.com>
2021-07-22 23:00:34 +0800

    新增通用功能

---
+++ src/Common.Utility/Cache/CacheKey.cs
@@ -0,0 +1,66 @@
+﻿using System.ComponentModel;
+
+namespace Common.Utility.Cache
+{
+    /// <summary>
+    /// 缓存键
+    /// </summary>
+    public static class CacheKey
+    {
+        /// <summary>
+        /// 登录验证码
+        /// <para>ADMIN:AUTH:VERIFYCODE:用户id</para>
+        /// </summary>
+        [Description("登录验证码")]
+        public const string VERIFYCODE_KEY = "ADMIN:AUTH:VERIFYCODE:{0}";
+
+        /// <summary>
+        /// 邮箱验证码
+        /// <para>ADMIN:EMAIL:VERIFYCODE:用户id</para>
+        /// </summary>
+        [Description("邮箱验证码")]
+        public const string EMAIL_VERIFYCODE_KEY = "ADMIN:EMAIL:VERIFYCODE:{0}";
+
+        /// <summary>
+        /// 手机号验证码
+        /// <para>ADMIN:PHONE:VERIFYCODE:用户id</para>
+        /// </summary>
+        [Description("手机号验证码")]
+        public const string PHONE_VERIFYCODE_KEY = "ADMIN:PHONE:VERIFYCODE:{0}";
+
+        /// <summary>
+        /// 刷新令牌 
+        /// <para>ADMIN:AUTH:REFRESHTOKEN:用户id</para>
+        /// </summary>
+        [Description("刷新令牌")]
+        public const string AUTH_REFRESH_TOKEN = "ADMIN:AUTH:REFRESHTOKEN:{0}";
+
+        /// <summary>
+        /// 账户信息
+        /// <para>ADMIN:ACCOUNT:INFO:用户id</para>
+        /// </summary>
+        [Description("账户信息")]
+        public const string ACCOUNT = "ADMIN:ACCOUNT:INFO:{0}";
+
+        /// <summary>
+        /// 用户权限
+        /// <para>ADMIN:ACCOUNT:PERMISSIONS:用户id</para>
+        /// </summary>
+        [Description("用户权限")]
+        public const string USER_PERMISSIONS = "ADMIN:ACCOUNT:PERMISSIONS:{0}";
+
+        /// <summary>
+        /// 微信登录session_id
+        /// <para>WX:SESSIONID:sessionid</para>
+        /// </summary>
+        [Description("微信登录")]
+        public const string WX_SESSIONID_KEY = "WX:SESSIONID:{0}";
+
+        /// <summary>
+        /// 微信openid
+        /// <para>WX:OPENID:openid</para>
+        /// </summary>
+        [Description("微信唯一标识符")]
+        public const string WX_OPENID_KEY = "WX:OPENID:{0}";
+    }
+}
---
+++ src/Common.Utility/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,143 @@
+﻿using Common.Utility.AOP;
+using Common.Utility.Attributes;
+using Common.Utility.Helper;
+using Common.Utility.UnitOfWork;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace Common.Utility.Extensions
+{
+    public static class ServiceCollectionExtension
+    {
+        /// <summary>
+        /// 从指定程序集中注入服务
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="assembly"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddServicesFromAssembly(this IServiceCollection services, Assembly assembly)
+        {
+            foreach (var type in assembly.GetTypes())
+            {
+                #region ==单例注入==
+
+                var singletonAttr = (SingletonAttribute)Attribute.GetCustomAttribute(type, typeof(SingletonAttribute));
+                if (singletonAttr != null)
+                {
+                    //注入自身类型
+                    if (singletonAttr.Itself)
+                    {
+                        services.AddSingleton(type);
+                        continue;
+                    }
+
+                    var interfaces = type.GetInterfaces().Where(m => m != typeof(IDisposable)).ToList();
+                    if (interfaces.Any())
+                    {
+                        foreach (var i in interfaces)
+                        {
+                            services.AddSingleton(i, type);
+                        }
+                    }
+                    else
+                    {
+                        services.AddSingleton(type);
+                    }
+
+                    continue;
+                }
+
+                #endregion
+
+                #region ==瞬时注入==
+
+                var transientAttr = (TransientAttribute)Attribute.GetCustomAttribute(type, typeof(TransientAttribute));
+                if (transientAttr != null)
+                {
+                    //注入自身类型
+                    if (transientAttr.Itself)
+                    {
+                        services.AddSingleton(type);
+                        continue;
+                    }
+
+                    var interfaces = type.GetInterfaces().Where(m => m != typeof(IDisposable)).ToList();
+                    if (interfaces.Any())
+                    {
+                        foreach (var i in interfaces)
+                        {
+                            services.AddTransient(i, type);
+                        }
+                    }
+                    else
+                    {
+                        services.AddTransient(type);
+                    }
+                    continue;
+                }
+
+                #endregion
+
+                #region ==Scoped注入==
+                var scopedAttr = (ScopedAttribute)Attribute.GetCustomAttribute(type, typeof(ScopedAttribute));
+                if (scopedAttr != null)
+                {
+                    //注入自身类型
+                    if (scopedAttr.Itself)
+                    {
+                        services.AddSingleton(type);
+                        continue;
+                    }
+
+                    var interfaces = type.GetInterfaces().Where(m => m != typeof(IDisposable)).ToList();
+                    if (interfaces.Any())
+                    {
+                        foreach (var i in interfaces)
+                        {
+                            services.AddScoped(i, type);
+                        }
+                    }
+                    else
+                    {
+                        services.AddScoped(type);
+                    }
+                }
+
+                #endregion
+            }
+
+            return services;
+        }
+
+        /// <summary>
+        /// 注入所有服务
+        /// </summary>
+        /// <param name="services"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddAllServices(this IServiceCollection services)
+        {
+            var assemblies = AssemblyHelper.Load();
+            foreach (var assembly in assemblies)
+            {
+                services.AddServicesFromAssembly(assembly);
+            }
+            return services;
+        }
+
+        /// <summary>
+        /// 添加数据库事务，在使用注解[Transaction]之前，必须添加该服务到容器中
+        /// </summary>
+        /// <param name="services"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddTransaction(this IServiceCollection services)
+        {
+            //https://www.cnblogs.com/sheng-jie/p/7416302.html
+            //services.AddScoped<IUnitOfWork, UnitOfWork<ContextMySql>>();
+            services.AddScoped(typeof(TransactionInterceptor));
+            return services;
+        }
+    }
+}
